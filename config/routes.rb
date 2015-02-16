b766e7488f01ce4cbae8222dbbf8fe9d0ddca3c7
lizhodgson <lizhodgson@gmail.com>
2015-02-16 10:34:31 -0500

    Adding flash alerts and notices

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,37 @@
-Rainforest::Application.routes.draw do
-  resources :products
-end
+Rails.application.routes.draw do
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  root 'products#index'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create]
+
+  resources :products do
+  resources :reviews, only: [:show, :create, :destroy]
+
+  end
+
+
+#   Rainforest::Application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+#   resources :products
+#   resources :users, only: [:new, :create]
+# end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
