b88b2e51592b1538eff7d74598579d641b0ffa53
lizhodgson <lizhodgson@gmail.com>
2015-02-16 08:46:11 -0500

    Generating the products controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
+Rainforest::Application.routes.draw do
+  resources :products
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
